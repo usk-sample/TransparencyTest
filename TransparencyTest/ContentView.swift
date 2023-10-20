55b4df200aa38ac7d6de1fd1f7aa11473c08c1df
Yuusuke Hasegawa <31399699+usk2000@users.noreply.github.com>
2023-10-20 14:10:25 +0900

    Initial Commit

---
+++ TransparencyTest/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TransparencyTest
+//
+//  Created by Yusuke Hasegawa on 2023/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TransparencyTest/TransparencyTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransparencyTestApp.swift
+//  TransparencyTest
+//
+//  Created by Yusuke Hasegawa on 2023/10/20.
+//
+
+import SwiftUI
+
+@main
+struct TransparencyTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
